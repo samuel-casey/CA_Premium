4bc828858e964a15e3e9266ecf917f9be580cde3
s-c <48266366+s-c-@users.noreply.github.com>
2020-06-23 10:13:21 -0400

    added NODE_ENV to .env, changed connection string in queries.js

--- queries.js
+++ queries.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 const Pool = require('pg').Pool
+
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
 })
 
 const stations_table = "station_metadata"
