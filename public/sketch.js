c7643b8e0d1fd5e3a40a35a5e79545f3f4fbe770
s-c <48266366+s-c-@users.noreply.github.com>
2020-06-24 14:48:11 -0400

    logging tideData.next_tide instead of its type

--- public/sketch.js
+++ public/sketch.js
@@ -344,7 +344,7 @@ function addTideEls(tideData) {
     const rangeSize = tideData.range_size
 
     console.log(`tideData: ${tideData}`)
-    console.log(typeof tideData.next_tide)
+    console.log(tideData.next_tide)
 
     const nextTideTimeString = nextTideObj["t"].trim()
 
