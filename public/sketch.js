3fb9b4b3e6805aeb144cfd6bddc4a399f53f75ae
s-c <48266366+s-c-@users.noreply.github.com>
2020-06-24 13:25:17 -0400

    log keys of tideData within dataLoaded

--- public/sketch.js
+++ public/sketch.js
@@ -336,7 +336,7 @@ function addTideEls(tideData) {
     const dataLoaded = async function (tideData) { return tideData }
     
     dataLoaded(tideData).then( () => {
-        console.log(`tideData bf nTO ${tideData}`)
+        console.log(`tideData bf nTO ${Object(tideData).keys()}`)
         nTOTest = tideData.next_tide
         console.log(nTOTest)
         console.log(typeof nTOTest)
